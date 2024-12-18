e38dee114975eda49bac150dbbffac8227f44bc5
SinanBalci10 <sinan.balci@my.com>
2024-12-18 21:27:37 +0100

    Änderungen beschrieben

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function checkAnswer() {
         document.getElementById('feedback').style.color = "blue";
     }
 }
-
+//dd
 // Funktion: Gehe zum nächsten Level
 function nextLevel() {
     currentLevel++;
